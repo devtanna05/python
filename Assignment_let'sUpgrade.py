9c79e90ca90397d72836d0931dc78a3e872ecb70
devtanna05 <93508859+devtanna05@users.noreply.github.com>
2024-01-04 07:53:28 +0530

    Create Assignment_let'sUpgrade.py

---
+++ Assignment_let'sUpgrade.py
@@ -0,0 +1,8 @@
+val = int(input("Enter a value to check is it Positive , Negitive or Zero :- "))
+
+if val == val>0 :
+	print("Number is Positive")
+elif val == val<0 :
+	print("Number is Negetive")
+else :
+	print("Number is Zero")
